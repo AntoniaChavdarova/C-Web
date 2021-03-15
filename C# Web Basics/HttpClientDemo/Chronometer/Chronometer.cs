eb4cb1c5defbc7c97112a65758d85f0dbde2b48b
AntoniaChavdarova <niaaachavdarova@abv.bg>
2021-03-15 14:10:12 +0200

    Refactoring

---
+++ C# Web Basics/HttpClientDemo/Chronometer/Chronometer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Chronometer
+{
+    public class Chronometer : Stopwatch , IChronometer
+    {
+        
+        private Stopwatch stopWatch;
+
+        public Chronometer()
+        {
+            this.stopWatch = new Stopwatch();
+            this.Laps = new List<string>();
+        }
+        public string GetTime => this.stopWatch.Elapsed.ToString();
+
+        public List<string> Laps { get; }
+
+        public string Lap()
+        {
+            
+            var lap = this.GetTime;
+            this.Laps.Add(lap);
+            this.stopWatch.Stop();
+            this.stopWatch.Start();
+
+            return lap;
+        }
+
+    
+
+        public void Reset()
+        {
+            this.stopWatch.Reset();
+        }
+
+        public void Start()
+        {
+            this.stopWatch.Start();
+            
+         
+
+        }
+
+        public void Stop()
+        {
+            this.stopWatch.Stop();
+        }
+    }
+}
---
+++ C# Web Basics/HttpClientDemo/Chronometer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chronometer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chronometer stopWatch1 = new Chronometer();
+
+            stopWatch1.Start();
+            stopWatch1.Reset();
+            Console.WriteLine(stopWatch1.GetTime);
+            stopWatch1.Lap();
+            stopWatch1.Lap();
+            Console.WriteLine(String.Join(Environment.NewLine , stopWatch1.Laps));
+            stopWatch1.Lap();
+            Thread.Sleep(1000);
+            Console.WriteLine(String.Join(Environment.NewLine, stopWatch1.Laps));
+
+             
+        }
+
+        
+    }
+}
